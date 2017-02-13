e3a00615cf866768c5143fe7192373063b6ae30b
DST-ftp-root <chendon@dstonline.com>
2017-02-13 00:58:56 -0500

    testing script commit

--- git_addCommit.bash
+++ git_addCommit.bash
@@ -7,5 +7,5 @@ cp -u -p /home/ec2-user/qnodes.out.* /mnt/ftp/httpd/customers/fccc/.
 
 
 # update github directory
-git --git-dir=/mnt/ftp/httpd/customers/fccc/.git --work-tree=/ add *
+git --git-dir=/mnt/ftp/httpd/customers/fccc/.git --work-tree=/ add -A
 git --git-dir=/mnt/ftp/httpd/customers/fccc/.git --work-tree=/ commit -am "Adding file"
