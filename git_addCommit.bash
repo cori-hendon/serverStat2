373bd68a4c578883880acae0b050b349c7a9be02
DST-ftp-root <chendon@dstonline.com>
2017-02-13 01:05:53 -0500

    automated file upload

--- git_addCommit.bash
+++ git_addCommit.bash
@@ -7,5 +7,9 @@ cp -u -p /home/ec2-user/qnodes.out.* /mnt/ftp/httpd/customers/fccc/.
 
 
 # update github directory
-git --git-dir=/mnt/ftp/httpd/customers/fccc/.git --work-tree=/ add -A
-git --git-dir=/mnt/ftp/httpd/customers/fccc/.git --work-tree=/ commit -am "Adding file"
+#git --git-dir=/mnt/ftp/httpd/customers/fccc/.git --work-tree=/ add -A
+#git --git-dir=/mnt/ftp/httpd/customers/fccc/.git --work-tree=/ commit -am "Adding file"
+
+git add -A
+git commit -am "automated file upload"
+git push origin master
